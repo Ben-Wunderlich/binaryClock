4b29f91b75ba8b74128feb84b53b4db4d73920cd
Ben Wunderlich <ben.wunderlich@outlook.com>
2019-11-03 19:59:17 -0800

    temp

--- clockHandling.js
+++ clockHandling.js
@@ -1,16 +1,19 @@
 
 function updateClock(){
-    var now = new Date();
-    var [days, hours, mins, secs] = weirdTime(now);
-    days = toBin(days);hours=toBin(hours);
+    var now = new Date()
+    var timeArr = weirdTime(now);
+
+    timeArr.forEach(function(element) {
+        console.log(element);
+    });
+    /*days = toBin(days);hours=toBin(hours);
     mins=toBin(mins);secs=toBin(secs);
-    var years = toBin(now.getFullYear());
 
     $("#secs").text(prependZeros(secs, 3));
     $("#mins").text(prependZeros(mins, 4));
     $("#hours").text(prependZeros(hours,5));
     $("#days").text(prependZeros(days, 9))
-    $("#years").text(years);
+    $("#years").text(years);*/
 
 
 }
@@ -20,16 +23,36 @@ function prependZeros(str, digits){
     return "0".repeat(zerosToAdd) + str;
 }
 
-function secondsThisYear(now){
-    var days = now.getDay();
-    var hours = now.getHours();
-    var mins = now.getMinutes();
-    var secs = now.getSeconds();
-    return (days*86400)+(hours*3600)+(mins*60)+secs
+function totalSeconds(now){
+    return now.getTime()/1000;
+}
+
+function powerProduct(num){
+    num/=2
+    var total = 0;
+    while(num>=2){
+        total*=num
+        num/=2
+    }
 }
 
 function weirdTime(now){
-    var yearSecs=secondsThisYear(now);
+    var allSecs = totalSeconds(now);
+    var currTime = 128
+    var tot=powerProduct(128);
+    var timeArr = [];
+    var min=2;
+    var result;
+    while(currTime >= min){
+        result = div(allSecs, tot);
+        tot/=currTime;
+        timeArr.push(toBin(result[1]));
+        allSecs = result[0];
+        currTime/=2;
+    }
+    return timeArr;
+
+    var yearSecs=totalSeconds(now);
     var [days, dayScraps] = div(yearSecs, 3456);
     var [hours, hourScraps] = div(dayScraps, 128);
     var [mins, secs] = div(hourScraps, 8);
